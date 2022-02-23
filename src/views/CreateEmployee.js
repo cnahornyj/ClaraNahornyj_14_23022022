0feb1c0825c4e8b8a2d392387cf3f5a589e4e79a
Clara <c.nahornyj@hotmail.fr>
2022-02-23 14:28:40 +0100

    Premier commit

---
+++ src/views/CreateEmployee.js
@@ -0,0 +1,10 @@
+function CreateEmployee() {
+  return (
+    <div>
+      <h1>Current Employees</h1>
+      <h2>Create Employee</h2>
+    </div>
+  );
+}
+
+export default CreateEmployee;
---
+++ src/views/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="title">
+      <h1>HRnet</h1>
+    </div>
+  );
+}
+
+export default Home;
