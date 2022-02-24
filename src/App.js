a6bf9a9405f3ede4166792bec18ae46d067bc29e
Clara <c.nahornyj@hotmail.fr>
2022-02-24 16:18:28 +0100

    Mise en place du router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './views/Home';
 import CreateEmployee from './views/CreateEmployee';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/createemployee" component={CreateEmployee} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
