193f56003730f56ca47f54f177a91c74cfd789a9
Clara <c.nahornyj@hotmail.fr>
2022-02-26 15:53:36 +0100

    Modifications des imports et routes après renommage du composant createEmployee -> currentEmployees

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './views/Home';
-import CreateEmployee from './views/CreateEmployee';
+import CurrentEmployees from './views/CurrentEmployees';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/createemployee" component={CreateEmployee} />
+        <Route path="/employees" component={CurrentEmployees} />
       </Switch>
     </BrowserRouter>
   );
