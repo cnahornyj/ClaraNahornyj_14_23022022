71e6431bb2a3b82e39cb6bf469f8ca1b1b9f8345
Clara <c.nahornyj@hotmail.fr>
2022-03-02 16:29:53 +0100

    Mise en place du filtre global sur le tableau des employés

---
+++ src/components/FilteringTable.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { useTable, useGlobalFilter } from "react-table";
+import mockUpData from "../mock-data.json";
+import { GlobalFilter } from "./GlobalFilter";
+
+function FilteringTable() {
+  const data = React.useMemo(() => mockUpData, []);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "First Name",
+        accessor: "firstName", // accessor is the "key" in the data
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+      },
+      {
+        Header: "Start Date",
+        accessor: "startDate",
+      },
+      {
+        Header: "Department",
+        accessor: "department",
+      },
+      {
+        Header: "Date Of Birth",
+        accessor: "dateOfBirth",
+      },
+      {
+        Header: "Street",
+        accessor: "street",
+      },
+      {
+        Header: "City",
+        accessor: "city",
+      },
+      {
+        Header: "State",
+        accessor: "state",
+      },
+      {
+        Header: "Zip Code",
+        accessor: "zipCode",
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable({ columns, data }, useGlobalFilter);
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()} style={{ border: "solid 1px #ccc" }}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    borderBottom: "solid 2px white",
+                    background: "#65748e",
+                    color: "white",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: "10px",
+                        border: "solid 1px gray",
+                        background: "#dfe6ed",
+                      }}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default FilteringTable;
--- src/views/CurrentEmployees.js
+++ src/views/CurrentEmployees.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Table from "../components/Table";
+import FilteringTable from "../components/FilteringTable";
 import data from "../mock-data.json";
 
 function CurrentEmployees() {
@@ -10,37 +10,7 @@ function CurrentEmployees() {
     <div>
       <h1>HRnet</h1>
       <h2>Current Employees</h2>
-      {/* <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Start Date</th>
-            <th>Department</th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip Code</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((employee, index) => (
-            <tr key={index}>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{employee.startDate}</td>
-              <td>{employee.department}</td>
-              <td>{employee.dateOfBirth}</td>
-              <td>{employee.street}</td>
-              <td>{employee.city}</td>
-              <td>{employee.state}</td>
-              <td>{employee.zipCode}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
-      <Table/>
+      <FilteringTable/>
       <Link to="/">Home</Link>
     </div>
   );
